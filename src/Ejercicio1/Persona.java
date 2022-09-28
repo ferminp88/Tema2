f0d3ba7948590223bcb864138118ce8b0e9f3e30
Fermin <ferminp88@gmail.com>
2022-09-28 14:29:58 -0300

    Ejercicio

---
+++ src/Ejercicio1/Persona.java
@@ -0,0 +1,24 @@
+package Ejercicio1;
+
+public class Persona {
+
+    public static void main(String[] args) {
+
+        int edad = 10;
+        String nombre = "Carlos";
+        String apellido = "Estevez";
+        boolean esmayor = true;
+        double altura = 1.20;
+        long   calzado = 35;
+        short  orejas = 2;
+
+        System.out.println("El edad es : "+edad );
+        System.out.println("El nombre es : "+nombre );
+        System.out.println("Es mayor ? : "+esmayor );
+        System.out.println("La altura es : "+altura );
+        System.out.println("Calzado =  "+calzado );
+        System.out.println("Cantidad de orejas : "+orejas );
+
+    }
+
+}
