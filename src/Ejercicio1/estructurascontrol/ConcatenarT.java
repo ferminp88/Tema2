a840fcbf71f487b8a9d88bd3aa5440d59ad691fa
Fermin <ferminp88@gmail.com>
2022-10-03 22:46:52 -0300

    Tema 3

---
+++ src/Ejercicio1/estructurascontrol/ConcatenarT.java
@@ -0,0 +1,12 @@
+package Ejercicio1.estructurascontrol;
+
+public class ConcatenarT {
+    public static void main(String[] args) {
+        String [] nombres = {"Javier",  "Luis", "Juan"};
+
+        for (int i = 0; i < nombres.length ; i++) {
+        System.out.print(" " + nombres[i]);
+
+        }
+    }
+}
